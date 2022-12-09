2da58aaeee6076259bc30f1ae79bf9888dda076d
Ivan Minakov <i.minakov786@gmail.com>
2022-12-09 10:01:13 -0500

    homework6 start

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,1 @@
-﻿double Factorial(int n)
-{
-    if (n == 1) return 1;
-    else return n * Factorial(n - 1);
-}
-for (int i = 0; i < 5; i++)
-{
-    System.Console.WriteLine($"{i}! = {Factorial(i)} ");
-}
+﻿
