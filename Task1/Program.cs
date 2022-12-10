1d81fb4eceefb4aca5bd1c1e2afaa256a1d06ac3
Ivan Minakov <i.minakov786@gmail.com>
2022-12-10 12:45:27 -0500

    task 1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿void CountNumbers()
+{
+    int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество элементов >0: {count}");
+}
+
+Console.Write("Задайте числовую последовательность: ");
+CountNumbers();
