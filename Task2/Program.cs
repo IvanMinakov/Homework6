48db8cbbb527383673d45d34983984ee2dcf876e
Ivan Minakov <i.minakov786@gmail.com>
2022-12-10 13:31:51 -0500

    done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CrossDots()
+{
+    System.Console.WriteLine("Введите точку b1");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите точку k1");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите точку b2");
+    double b2 = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите точку k2");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Math.Round(x, 2);
+    Math.Round(y, 2);
+
+    System.Console.WriteLine($"Пересечения в точке ({x} ; {y}) ");
+}
+
+System.Console.WriteLine("Ниже ведите координаты точек для проверки пересечения");
+System.Console.WriteLine();
+CrossDots();
